dc8f8cf6a368891caa470428bc576ffd62a5ff12
Ahmed <ahmedmahmo431@gmail.com>
2026-03-17 18:00:29 +0200

    Refactor: Database operations abstraction

--- controllers/note.php
+++ controllers/note.php
@@ -8,14 +8,8 @@ $db = new Database($config['database']);
 
 $note = $db->query('SELECT * FROM notes WHERE id = :id', [
     'id' => $_GET['id']
-])->fetch();
+])->findOrFail();
 
-if(!$note){
-    abort(Response::NOT_FOUND);
-}
-
-if($note['user_id'] !== $currentUserId){
-    abort(Response::FORBIDDEN);
-}
+authorize($note['user_id'] !== $currentUserId);
 
 require './views/note.view.php';
