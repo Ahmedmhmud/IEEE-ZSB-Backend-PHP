e9ee6475f5dff5a0c5f2ec735448b297c5f2dbfa
Ahmed <ahmedmahmo431@gmail.com>
2026-03-17 22:28:46 +0200

    Refactor: simple validator class

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,5 +1,6 @@
 <?php
 
+require "./Validator.php";
 $config = require "./config.php";
 $db = new Database($config['database']);
 
@@ -7,8 +8,8 @@ $heading = 'Create Note';
 
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
     $errors = [];
-    if(strlen($_POST['body']) === 0){
-        $errors['body'] = 'Body is required';
+    if(! Validator::string($_POST['body'], 1, 1000)){
+        $errors['body'] = 'A body of no more than 1000 characters is required';
     }
 
     if(strlen($_POST['body']) > 1000){
