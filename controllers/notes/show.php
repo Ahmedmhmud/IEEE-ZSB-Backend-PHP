87e3668ec6176d01f2e1a46196da001d070e9780
Ahmed <ahmedmahmo431@gmail.com>
2026-05-08 11:49:01 +0300

    adding delete button for notes via POST method

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -2,17 +2,33 @@
 
 use Core\Database;
 
-$currentUserId = 1;
-
 $config = require base_path('config.php');
 $db = new Database($config['database']);
 
-$note = $db->query('SELECT * FROM notes WHERE id = :id', [
-    'id' => $_GET['id']
-])->findOrFail();
+$currentUserId = 1;
 
-authorize($note['user_id'] !== $currentUserId);
 
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $note = $db->query('SELECT * FROM notes WHERE id = :id', [
+        'id' => $_GET['id']
+    ])->findOrFail();
+
+    authorize($note['user_id'] !== $currentUserId);
+    
+    $db->query('DELETE FROM notes WHERE id = :id', [
+        'id' => $_POST['id']
+    ]);
+
+    header("location: /notes");
+    exit();
+} else {
+    $note = $db->query('SELECT * FROM notes WHERE id = :id', [
+        'id' => $_GET['id']
+    ])->findOrFail();
+
+    authorize($note['user_id'] !== $currentUserId);
+}
+    
 view('notes/show.view.php', [
     'heading' => 'Note',
     'note' => $note
