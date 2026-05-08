d5562dafce031135bd7fbb2d6883fd1622d32e75
Ahmed <ahmedmahmo431@gmail.com>
2026-05-08 18:31:10 +0300

    updating notes resource

---
+++ controllers/notes/update.php
@@ -0,0 +1,36 @@
+<?php
+
+use Core\App,
+    Core\Database,
+    Core\Validator;
+
+$db = App::resolve(Database::class);
+
+$currentUserId = 1;
+
+$note = $db->query('SELECT * FROM notes WHERE id = :id', [
+    'id' => $_POST['id']
+])->findOrFail();
+
+authorize($note['user_id'] !== $currentUserId);
+
+$errors = [];
+if(! Validator::string($_POST['body'], 1, 1000)){
+    $errors['body'] = 'A body of no more than 1000 characters is required';
+}
+
+if (count($errors)) {
+    return view('notes/edit.view.php', [
+        'heading' => 'Edit Note',
+        'errors' => $errors,
+        'note' => $note
+    ]);
+}
+
+$db->query('UPDATE notes SET body = :body WHERE id = :id', [
+    'body' => $_POST['body'],
+    'id' => $_POST['id']
+]);
+
+header('location: /notes');
+die();
