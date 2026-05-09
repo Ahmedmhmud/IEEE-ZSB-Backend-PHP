872732fc19c68348f02f6b201277ef8381d1f813
Ahmed <ahmedmahmo431@gmail.com>
2026-05-09 04:14:02 +0300

    log in & out

--- Core/functions.php
+++ Core/functions.php
@@ -22,6 +22,20 @@ function abort($code = 404) {
         die();
 }
 
+function login($user) {
+    $_SESSION['user'] = [
+        'email' => $user['email']
+    ];
+}
+
+function logout() {
+    $_SESSION = [];
+    session_destroy();
+
+    $params = session_get_cookie_params();
+    setcookie('PHPSESSID', '', time() - 3600, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+}
+
 function authorize($condition, $status = Response::FORBIDDEN){
     if($condition){
         abort($status);
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -10,7 +10,9 @@
                 <!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
                 <a href="/" aria-current="page" class="<?= urlIs('/')? "rounded-md bg-gray-950/50 px-3 py-2 text-sm font-medium text-white":"rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white" ?>">Home</a>
                 <a href="/about" class="<?= urlIs('/about')? "rounded-md bg-gray-950/50 px-3 py-2 text-sm font-medium text-white":"rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white" ?>">About</a>
-                <a href="/notes" class="<?= urlIs('/notes')? "rounded-md bg-gray-950/50 px-3 py-2 text-sm font-medium text-white":"rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white" ?>">Notes</a>
+                <?php if ($_SESSION['user'] ?? false) : ?>
+                    <a href="/notes" class="<?= urlIs('/notes')? "rounded-md bg-gray-950/50 px-3 py-2 text-sm font-medium text-white":"rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white" ?>">Notes</a>
+                <?php endif; ?>
                 <a href="/contact" class="<?= urlIs('/contact')? "rounded-md bg-gray-950/50 px-3 py-2 text-sm font-medium text-white":"rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white" ?>">Contact</a>
             </div>
         </div>
@@ -34,7 +36,8 @@
                 <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="size-8 rounded-full outline -outline-offset-1 outline-white/10" />
             </button>
             <?php else : ?>
-                <a href='/register'>Register</a>
+                <a href="/register" class="<?= urlIs('/register')? "rounded-md bg-gray-950/50 px-3 py-2 text-sm font-medium text-white":"rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white" ?>">Register</a>
+                <a href="/login" class="<?= urlIs('/login')? "rounded-md bg-gray-950/50 px-3 py-2 text-sm font-medium text-white":"rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white" ?>">Log In</a>
             <?php endif; ?>
             <el-menu anchor="bottom end" popover class="w-48 origin-top-right rounded-md bg-gray-800 py-1 outline-1 -outline-offset-1 outline-white/10 transition transition-discrete [--anchor-gap:--spacing(2)] data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in">
                 <a href="#" class="block px-4 py-2 text-sm text-gray-300 focus:bg-white/5 focus:outline-hidden">Your profile</a>
@@ -44,6 +47,15 @@
             </el-dropdown>
         </div>
         </div>
+
+        <?php if ($_SESSION['user'] ?? false) : ?>
+            <div class="ml-3">
+                <form method="POST" action="/sessions">
+                    <input type="hidden" name="_method" value="DELETE">
+                    <button>Log Out</button>
+                </form>
+            </div>
+        <?php endif; ?>
         <div class="-mr-2 flex md:hidden">
         <!-- Mobile menu button -->
         <button type="button" command="--toggle" commandfor="mobile-menu" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500">
