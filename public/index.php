b7d4fde188d4d9cc5c76e5ef1b3e8491e80e0c4e
Ahmed <ahmedmahmo431@gmail.com>
2026-05-12 21:10:42 +0300

    feat: installing composer

--- public/index.php
+++ public/index.php
@@ -1,19 +1,18 @@
 <?php
 
+const BASE_PATH = __DIR__ . '/../';
+
+require BASE_PATH . '/vendor/autoload.php';
+
 session_start();
 
 use Core\Router,
     Core\Session,
     Core\ValidationException;
 
-const BASE_PATH = __DIR__ . '/../';
 
 require BASE_PATH . 'Core/functions.php';
 
-spl_autoload_register(function($class){
-    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-    require base_path("{$class}.php");
-});
 
 require base_path('bootstrap.php');
 
